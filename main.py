5753693911324d0d2a9b4360b804b01c015434bf
ixor911 <igorbutenko9111@gmaul.com>
2022-07-29 18:52:01 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(('data.pr4e.org', 80))
+request = 'GET http://data.pr4e.org/page1.htm HTTP/1.1 \r\n\r\n'.encode()
+sock.send(request)
+
+while True:
+    data = sock.recv(512)
+    if len(data) < 1:
+        break
+    print(data.decode(), end=' ')
+
+sock.close()
